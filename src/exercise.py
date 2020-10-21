cf58caa481904132cd8b761d42e5ae522ea0e8a8
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 19:31:29 +0000

    exe

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
-    #write your code below this line
+    division(10,2)
+
+def division(numerator, denominator):
+    print(numerator/denominator)
 
 if __name__ == '__main__':
     main()
